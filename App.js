80d307f7aec37c0cbe49b1e2c98a554d33866bb9
Vinh <luongvinh285@gmail.com>
2021-12-18 10:18:55 +0700

    hello

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Form1 from "./form/form1"
+import Form2 from "./form/form2"
+import Form3 from "./form/form3"
+import { NavigationContainer } from '@react-navigation/native';
+export default function App() {
+  return (
+    <Form2/>
+  )
+}
